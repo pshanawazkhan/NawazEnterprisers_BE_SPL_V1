e2865dee529cb325468e0b39031512e81c8e39fc
pshanawazkhan <9490549755gmail>
2019-08-17 17:51:05 +0530

    COM

--- src/main/java/com/nawaz/splinkers/logic/GitTesting.java
+++ src/main/java/com/nawaz/splinkers/logic/GitTesting.java
@@ -16,13 +16,13 @@ public class GitTesting {
 		
 	}
 	
-void goodBad1() {
+void goodBad() {
 		
 		
 		for(int x=10;x<15;x++) {
 			
 			System.out.println("-------bad ---");
-			System.out.println("FILES");
+			
 		}
 		
 	
