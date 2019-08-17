ce93c67c76472d3e7edb73d3ecbb505df4584774
pshanawazkhan <9490549755gmail>
2019-08-17 16:55:47 +0530

    test

---
+++ src/main/java/com/nawaz/splinkers/logic/GitTesting.java
@@ -0,0 +1,22 @@
+package com.nawaz.splinkers.logic;
+
+public class GitTesting {
+
+	
+	
+	void gTest() {
+		
+		
+		System.out.println("adding to test the files");
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
