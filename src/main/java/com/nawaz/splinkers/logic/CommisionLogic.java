54df7e7c3cfc587e4e0726a53eb001c34335c551
pshanawazkhan <9490549755gmail>
2019-08-17 16:48:03 +0530

    commit

---
+++ src/main/java/com/nawaz/splinkers/logic/CommisionLogic.java
@@ -0,0 +1,65 @@
+package com.nawaz.splinkers.logic;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommisionLogic {
+
+	
+	
+	
+	
+	public Map<String, Object> search(Form form) {
+		
+		Map<String, Object> m1= new HashMap<>();
+		
+		AllForms allf = new AllForms();
+		
+		allf.setSparte(form.getSparte());
+		allf.setCommisionTo(form.getCommisionTo());
+		
+		List<String> commups = form.getCommisionUplode();
+		
+		if(commups != null && commups.size() >1) {
+			
+			allf.setAllcommisionUplode(commups);
+			
+		}
+		
+		if(commups != null && commups.size() == 1) {
+			
+			
+			allf.setCommisionFrom(commups.get(0));
+			
+		}
+		m1.put("Sparte", allf.getSparte());
+		m1.put("commision No TO", allf.getCommisionTo());
+		m1.put("commision No fROM", allf.getCommisionFrom());
+		m1.put("uplode Commision No", allf.getAllcommisionUplode());
+		
+		
+		System.out.println("------------------------output ----------------------------");
+		System.out.println("allf.getCommisionFrom()--> "+allf.getCommisionFrom());
+		System.out.println("allf.getSparte()--> "+allf.getSparte());
+		System.out.println("allf.getCommisionTo()--> "+allf.getCommisionTo());
+		System.out.println("allf.getCommisionFrom()--> "+allf.getCommisionFrom());
+		System.out.println("allf.getAllcommisionUplode()--> "+allf.getAllcommisionUplode());
+		
+		System.out.println("----------------------------------------------------------------");
+		
+		return m1;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/com/nawaz/splinkers/logic/Form.java
@@ -0,0 +1,59 @@
+package com.nawaz.splinkers.logic;
+
+import java.util.List;
+
+
+public class Form {
+
+	
+	private String sparte;
+	
+	private String commisionTo;
+	
+	private String commisionFrom;
+	
+	private List<String> commisionUplode;
+
+	public String getSparte() {
+		return sparte;
+	}
+
+	public void setSparte(String sparte) {
+		this.sparte = sparte;
+	}
+
+	public String getCommisionTo() {
+		return commisionTo;
+	}
+
+	public void setCommisionTo(String commisionTo) {
+		this.commisionTo = commisionTo;
+	}
+
+	public String getCommisionFrom() {
+		return commisionFrom;
+	}
+
+	public void setCommisionFrom(String commisionFrom) {
+		this.commisionFrom = commisionFrom;
+	}
+
+	public List<String> getCommisionUplode() {
+		return commisionUplode;
+	}
+
+	public void setCommisionUplode(List<String> commisionUplode) {
+		this.commisionUplode = commisionUplode;
+	}
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
